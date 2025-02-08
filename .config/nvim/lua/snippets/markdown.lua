c6b751daa9fd70780e92a070e14e5e604f7c929e
Samuel Olagunju <me@samola.net>
2025-02-08 17:57:35 -0500

    Added markdown table snippet to neovim

---
+++ .config/nvim/lua/snippets/markdown.lua
@@ -0,0 +1,14 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+
+ls.add_snippets("markdown", {
+  s("table", {
+    t({
+      "| Column 1 | Column 2 | Column 3 |",
+      "|----------|----------|----------|",
+      "| Data 1   | Data 2   | Data 3   |",
+      "| Data 4   | Data 5   | Data 6   |",
+    }),
+  }),
+})
