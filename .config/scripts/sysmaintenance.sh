b13ea9b7ad51171b95b8005c27e70be9bc341028
Samuel Olagunju <me@samola.net>
2025-03-04 19:35:38 -0500

    Update sysmaintenance script again

--- .config/scripts/sysmaintenance.sh
+++ .config/scripts/sysmaintenance.sh
@@ -1,19 +1,24 @@
-# This code is almost entirely taken from Mr. Cejas's blog: https://fernandocejas.com/blog/engineering/2022-03-30-arch-linux-system-maintance/
-echo "Updating system"
+# Based onMr. Cejas's blog, with some minor adjustments for displaying: https://fernandocejas.com/blog/engineering/2022-03-30-arch-linux-system-maintance/
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+NC="\033[0m" # No Color
+
+echo -e "${YELLOW}Updating system${NC}\n"
 yay -Syu
 
-echo "Clearing pacman cache"
+echo -e "${YELLOW}Clearing pacman cache${NC}\n"
 pacman_cache_space_used="$(du -sh /var/cache/pacman/pkg/)"
-paccache -r 
-echo "Space saved: $pacman_cache_space_used" 
+# need pacman-contrib package
+paccache -r
+echo -e "${GREEN}Space saved: $pacman_cache_space_used${NC}\n"
 
-echo "Removing orphan packages"
+echo -e "${YELLOW}Removing orphan packages${NC}\n"
 yay -Qdtq | yay -Rns -
 
-echo "Clearing ~/.cache"
+echo -e "${YELLOW}Clearing ~/.cache${NC}\n"
 home_cache_used="$(du -sh ~/.cache)"
 rm -rf ~/.cache/
-echo "Spaced saved: $home_cache_used"
+echo -e "${GREEN}Spaced saved: $home_cache_used${NC}\n"
 
-echo "Clearing system logs"
-journalctl --vacuum-time=7d
+echo -e "${YELLOW}Clearing system logs${NC}\n"
+sudo journalctl --vacuum-time=7d
